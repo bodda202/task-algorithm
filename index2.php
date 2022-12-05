9563def0c7099bc27c0ae45326447e83a5ba205b
bodda202 <119898140+bodda202@users.noreply.github.com>
2022-12-05 23:56:40 +0200

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,98 @@
+<?php
+// Insertion sort (php)
+function insert($my_array)
+{
+    for($i=0;$i<count($my_array);$i++){
+        $val = $my_array[$i];
+        $j = $i-1;
+        while($j>=0 && $my_array[$j] > $val){
+            $my_array[$j+1] = $my_array[$j];
+            $j--;
+        }
+        $my_array[$j+1] = $val;
+    }
+return $my_array;
+}
+$test_array = array(3, 0, 2, 5, -1, 4, 1);
+echo "Original Array :\n";
+echo implode(', ',$test_array );
+echo "\nSorted Array :\n";
+print_r(insert($test_array));
+
+// merge sort in PHP
+$array = array(8,1,2,5,6,7);
+print_array($array);
+merge_sort($array);
+print_array($array);
+
+function merge_sort(&$list){
+    if( count($list) <= 1 ){
+        return $list;
+    }
+
+    $left =  array();
+    $right = array();
+
+    $middle = (int) ( count($list)/2 );
+
+    // Make left
+    for( $i=0; $i < $middle; $i++ ){
+        $left[] = $list[$i];
+    }
+
+    // Make right
+    for( $i = $middle; $i < count($list); $i++ ){
+        $right[] = $list[$i];
+    }
+
+    // Merge sort left & right
+    merge_sort($left);
+    merge_sort($right);
+
+    // Merge left & right
+    return merge($left, $right);
+}
+
+function merge(&$left, &$right){
+    $result = array();
+
+    while(count($left) > 0 || count($right) > 0){
+        if(count($left) > 0 && count($right) > 0){
+            if($left[0] <= $right[0]){
+                $result[] = array_shift($left);
+            } else {
+                $result[] = array_shift($right);
+            }
+        } elseif (count($left) > 0){
+            $result[] = array_shift($left);
+        } elseif (count($right) > 0){
+            $result[] = array_shift($right);
+        }
+    }
+
+    print_array($result);exit;
+
+    return $result;
+}
+
+function print_array($array){
+    echo "<pre>";
+    print_r($array);
+    echo "<br/>";
+    echo "</pre>";
+}
+    ?>
+
+
+
+ 
+
+ 
+
+
+
+
+
+
+
+
